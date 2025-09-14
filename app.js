7ed645fa39384ab0779446c090efa6aad0fcd620
sijomonps <sijomon700@gmail.com>
2025-09-14 22:02:59 +0530

    UI completed and need to fix some error in the JS

---
+++ app.js
@@ -0,0 +1,7 @@
+let outputEl = document.querySelector("#output-heading");
+let inputBillEl = document.getElementById("bill");
+let inputPeopleEl = document.getElementById("people");
+
+outputEl.textContent = "hi";
+
+console.log("hi");
